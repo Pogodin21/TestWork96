985295f608edaa3cb6f9b009c5e41f491a1b1b38
Pogodin Eduard <pogodin.eduard.m@gmail.com>
2025-10-03 20:35:02 +0400

    feat(login-page): add login page markup and styles

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,24 @@
 'use client';
 
+import styles from './login.module.scss';
+
 export default function Login() {
-    return (
-        <div></div>
-    )
+  return (
+    <section className={styles.login}>
+      <div className='container'>
+          <h2 className={styles.login__title}>Login</h2>
+          <form className={styles.login__form}>
+            <div className={styles.login__field}>
+              <input type="text" className={styles.login__input} placeholder="Username" />
+            </div>
+    
+            <div className={styles.login__field}>
+              <input type="password" className={styles.login__input} placeholder="Password" />
+            </div>
+    
+            <button className={styles.login__button}>Login</button>
+          </form>
+      </div>
+    </section>
+  );
 }
